3e9e92e9b87054b05e9ceadd2228a4451e498399
fatfodge <rogerhatfield18@gmail.com>
2025-07-14 13:25:48 -0500

    1.7.1
    profile panel

--- ui.js
+++ ui.js
@@ -3,6 +3,25 @@ import { setBottomPanelState } from "./swipeHandler.js";
 export function initUI() {
     try {
         let addressInput = document.getElementById('address-input');
-        addressInput.addEventListener('click', () => setBottomPanelState('max'))
-    } catch { console.log("init UI failed"); }
+        let profileBtn = document.getElementById('profile-btn');
+        let profileCloseBtn = document.getElementById('close-profile-container');
+
+        addressInput.addEventListener('click', () => setBottomPanelState('max'));
+        profileBtn.addEventListener('click', () => toggleProfileContainer());
+        profileCloseBtn.addEventListener('click', () => toggleProfileContainer());
+    } catch { console.log("init UI failed"); };
+}
+
+function toggleProfileContainer() {
+    let profileContainer = document.getElementById('profile-container');
+    let loginForm = document.getElementById('login-form');
+    let activeUserForm = document.getElementById('activeUserFrom');
+    let adminPanel = document.getElementById('adminPanelSheet');
+
+    if (profileContainer.classList.contains('expanded')) {
+        profileContainer.classList.remove('expanded');
+    } else {
+        profileContainer.classList.add('expanded');
+        setBottomPanelState('min');
+    }
 }
